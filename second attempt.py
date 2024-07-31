9961735676ed32489f1c3b4ef3596e71e194bccf
Ben <82953917+chocolatebrioche@users.noreply.github.com>
2024-07-31 11:49:34 +0100

    Fix function that return unstable vertices

--- second attempt.py
+++ second attempt.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-n = 5
+# Initial setup of nxn grid
+n = 15
 m = n // 2
 #n odd is more useful as I have an origin
 
@@ -10,7 +11,7 @@ grid = np.zeros((n,n))
 
 
 
-# Be able to use cartesian coordinates 
+# Converting to and from cartesian coordinates 
         
 def cartToMatrix(point):
     x = point[0]
@@ -23,6 +24,8 @@ def matrixToCart(point):
     y = point[1]
     return (y-m, m-x)
 
+
+
 # Setting invalid points to -1
 for i in range(n):
     for j in range(n):
@@ -30,15 +33,6 @@ for i in range(n):
         x, y = matrixToCart(point)
         if x**2 + y**2 > m**2:
             grid[i,j] = -1
-
-# Add/remove at certain coordinate, using matrix coordinates
-
-def addAt(point):
-    x, y = point[0], point[1]
-    grid[x,y] += 1
-def removeAt(point):
-    x, y = point[0], point[1]
-    grid[x,y] -= 1
 
 # Return degree of a vertex, using matrix coordinates
 def degree(point):
@@ -51,6 +45,29 @@ def degree(point):
         return 4
 
 
+
+# Add/remove at certain coordinate, using matrix coordinates         
+    
+def addAt(point):
+    x, y = point[0], point[1]
+    grid[x,y] += 1
+def removeAt(point):
+    x, y = point[0], point[1]
+    grid[x,y] -= 1
+
+
+addAt(cartToMatrix((0,0)))
+addAt(cartToMatrix((0,0)))
+addAt(cartToMatrix((0,0)))
+addAt(cartToMatrix((0,0)))
+addAt(cartToMatrix((0,0)))
+addAt(cartToMatrix((0,0)))
+addAt(cartToMatrix((0,0)))
+addAt(cartToMatrix((7,0)))
+addAt(cartToMatrix((7,0)))
+addAt(cartToMatrix((7,0)))
+addAt(cartToMatrix((7,0)))
+print(grid)
 
 
 # Topple at certain coordinate, using matrix coordinates ADD THE UNSTABLE RETURN ARRAY
@@ -76,18 +93,22 @@ def toppleAt(point):
                 if grid[newPoint[0],newPoint[1]] >= degree(newPoint):
                     unstable.append(newPoint)
     return unstable
-            
-            
-addAt(cartToMatrix((0,0)))
-addAt(cartToMatrix((0,0)))
-addAt(cartToMatrix((0,0)))
-addAt(cartToMatrix((0,0)))
-addAt(cartToMatrix((0,0)))
-toppleAt(cartToMatrix((0,0)))
-addAt(cartToMatrix((0,0)))
-addAt(cartToMatrix((0,0)))
-addAt(cartToMatrix((0,0)))
-toppleAt(cartToMatrix((0,0)))
-print(grid)
 
-    #return the unstable points 
+
+# For now, use a brute-force method to find the unstable vertices i.e. creating mask, and then scan through that always toppling the first one that is True
+# This will be slow but should work for now
+
+# This will return a list of the unstable vertices in matrix coordinates format, not tuples in 2d-array format
+
+def unstable():
+    arr = np.zeros((n,n))
+    for i in range(0,n):
+        for j in range(0,n):
+            if grid[i,j] != -1:
+                arr[i,j] = grid[i,j] >= degree((i,j))
+            else:
+                arr[i,j] = False
+    return np.argwhere(arr == 1)
+
+print(unstable())
+
